1aba4f6bf060cff3b94ebef30e1c6b241bc6ebf0
victornaito <alphabeta>
2017-10-09 20:42:32 -0400

    .

--- src/WSTest.java
+++ src/WSTest.java
@@ -1,6 +1,10 @@
+import example.HelloWorld;
+
 public class WSTest {
     public static void main (String [] args){
-
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println(helloWorld.sayHelloWorldFrom("Victor"));
+        
     }
 
 }
--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -1,7 +1,7 @@
 package example;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
-import javax.xml.ws.Endpoint;
 
 @WebService()
 public class HelloWorld {
@@ -11,9 +11,9 @@ public class HelloWorld {
     System.out.println(result);
     return result;
   }
-  public static void main(String[] argv) {
-    Object implementor = new HelloWorld ();
-    String address = "http://localhost:9000/HelloWorld";
-    Endpoint.publish(address, implementor);
-  }
+//  public static void main(String[] argv) {
+//    Object implementor = new HelloWorld ();
+//    String address = "http://localhost:9000/HelloWorld";
+//    Endpoint.publish(address, implementor);
+//  }
 }
